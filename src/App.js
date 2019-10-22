2059559475f315597b8447ab215841381322de37
Shrutika <you@example.com>
2019-10-23 01:15:37 +0530

    update routes and navbar

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
-import NavBar from './component/navbar';
 import FacebookLogin from 'react-facebook-login';
 import GoogleLogin from 'react-google-login';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Home from './pages/home';
 
 class App extends Component {
-
   render() {
+            const responseFacebook = (response) => {
+              console.log(response);
+            }
 
-    const responseFacebook = (response) => {
-      console.log(response);
-    }
-
-    const responseGoogle = (response) => {
-      console.log(response);
-    }
+            const responseGoogle = (response) => {
+              console.log(response);
+            }
 
     return (
-      <div className="App">
-        <NavBar/>
-        <h1>LOGIN WITH FACEBOOK AND GOOGLE</h1>
-      <FacebookLogin
-        appId="" //APP ID NOT CREATED YET
-        fields="name,email,picture"
-        callback={responseFacebook}
-      />
-      <br />
-      <br />
-
-      
-      <GoogleLogin
-        clientId="" //CLIENTID NOT CREATED YET
-        buttonText="LOGIN WITH GOOGLE"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-      />
-
-      </div>
+            <div className="App">
+             
+              <Home/>
+             
+                <h1>LOGIN WITH FACEBOOK AND GOOGLE</h1>
+                <FacebookLogin
+                    appId="" //APP ID NOT CREATED YET
+                    fields="name,email,picture"
+                    callback={responseFacebook}
+                />
+                <br />
+                <br />
+                <GoogleLogin
+                    clientId="" //CLIENTID NOT CREATED YET
+                    buttonText="LOGIN WITH GOOGLE"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                />
+            </div>
     );
   }
 }
