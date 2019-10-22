8468f746e79d00a136c98a7cc0b39a057a15d99a
Shrutika <you@example.com>
2019-10-23 01:11:33 +0530

    update home page with routes and navbar

---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Routes from '../routes/route';
+
+
+class Home extends Component{
+    render(){
+        return(
+            <Routes/>  
+        )
+
+        
+    }
+};
+
+export default Home;
